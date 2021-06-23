3cd3bb25c7859b662ce6a6052253eba6ef99ebe7
kanhaiya <kumarkhg4@gmail.com>
2021-06-23 13:34:43 +0530

    Basic frontend part

---
+++ client/src/components/profile/ProfileTabContent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { UserCard } from "../follow/FollowPagecontent";
+import Article from "../home/Article";
+
+
+
+
+const ProfileTabContent = ({profile,tabstate})=>{
+    return (
+        <div className={"profile_tab_content"+(tabstate===0?" additional_style":"")}>
+            {tabstate===0&&profile.stories.map(story=><Article article={story}/>)}
+            {tabstate===1&&profile.followers.map(user=><UserCard people={user}/>)}
+            {tabstate===2&&
+                <div className="profile_about">
+                    <div className="profile_content">
+                        <h2> About {profile.username}</h2>
+                        <div>{profile.about}</div>
+                    </div>
+                    <div className="profile_avatar">
+                        <img src={profile.avatar} alt=""/>
+                    </div>
+                </div>}
+        </div>
+    )
+}
+
+export default ProfileTabContent;
